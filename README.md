8ae1e202f0ba50c0f0489bd82b74c63b226f82c7
Twoje Imię <lobrusik@gmail.com>
2026-04-09 15:32:39 +0200

    Wymagania na 4

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 W ramach pierwszego zadania proszę wykonać grę kółko i krzyżyk w
 Bashu, który:<br>
 ✅ 3.0 - działa w trybie gry turowej,<br>
-❌ 4.0 - pozwala na zapis i odtwarzanie przerwanej gry (save game),<br>
+✅ 4.0 - pozwala na zapis i odtwarzanie przerwanej gry (save game),<br>
 ❌ 5.0 - pozwala na grę z komputerem.
--- tictactoe/tictactoe.sh
+++ tictactoe/tictactoe.sh
@@ -2,6 +2,7 @@
 
 board=(" " " " " " " " " " " " " " " " " ")
 player="X"
+save="tictactoe_save.txt"
 
 
 draw_board() {
@@ -31,10 +32,61 @@ if_win() {
     return 1
 }
 
+save_game(){
+    local data=""
+    for (( i=0; i<9; i++ ))
+    do
+        if [[ "${board[$i]}" == " " || "${board[$i]}" == "" ]]
+        then
+            data="${data}."
+        else
+            data="${data}${board[$i]}"
+        fi
+    done
+
+    echo "$data" > "$save"
+    echo "$player" >> "$save"
+    echo "Zapisano grę"
+    exit 0
+}
+
+load_game(){
+    if [[ -f "$save" ]]
+    then
+        local start_board=$(head -n 1 "$save")
+
+        for (( i=0; i<9; i++ ))
+        do
+            char="${start_board:$i:1}"
+            if [[ "$char" == "." ]]
+            then
+                board[$i]=" "
+            else
+                board[$i]="$char"
+            fi
+        done
+
+        player=$(sed -n '2p' "$save")
+
+        echo "Wczytano poprzednią grę."
+    else
+        echo "Brak zapisanych rozgrywek."
+        sleep 1
+    fi
+}
+
+echo "Czy wczytać zapisana grę? (t/n)"
+read -r choice
+[[ "$choice" == "t" ]] && load_game
+
 while true; 
     do
         draw_board
-        read -p "Gracz $player wybiera pole (1-9): " input
+        read -p "Gracz $player wybiera pole (1-9) lub, s, by zapisać i wyjść: " input
+        if [[ "$input" == "s" || "$input" == "S" ]]
+        then
+            save_game
+        fi
 
         idx=$((input-1))
         if [[ ! "$input" =~ ^[1-9]$ ]] || [[ "${board[$idx]}" != " " ]] 
@@ -50,6 +102,7 @@ while true;
         then
             draw_board
             echo "Gracz $player wygrywa!"
+            rm -f "$save"
             break
         fi
 
@@ -66,6 +119,7 @@ while true;
         then
             draw_board
             echo "REMIS!"
+            rm -f "$save"
             break
         fi
 
